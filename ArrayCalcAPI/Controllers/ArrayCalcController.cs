94c34339a794ff6a710869230f32b9b67b50f28b
rajinder1801 <48193949+rajinder1801@users.noreply.github.com>
2019-06-26 20:52:46 +1000

    Git Commit

---
+++ ArrayCalcAPI/Controllers/ArrayCalcController.cs
@@ -0,0 +1,72 @@
+﻿using ArrayCalcContracts;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+namespace ArrayCalcAPI.Controllers
+{
+    /// <summary>
+    /// Array Calc Controller.
+    /// </summary>
+    [RoutePrefix("api/arraycalc")]
+    public class ArrayCalcController : ApiController
+    {
+        private IArrayOperations arrayOperations;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="arrayoperations"></param>
+        public ArrayCalcController(IArrayOperations arrayoperations)
+        {
+            this.arrayOperations = arrayoperations;
+        }
+
+        /// <summary>
+        /// Http Get method for reverse array.
+        /// </summary>
+        /// <param name="productIds">productIds</param>
+        /// <returns>Reversed Array</returns>
+        [HttpGet]
+        [Route("Reverse")]
+        public HttpResponseMessage Reverse([FromUri] int[] productIds)
+        {
+            if (productIds == null)
+                return new HttpResponseMessage()
+                {
+                    Content = new StringContent("Invalid request", Encoding.UTF8, "text/html")
+                };
+
+            var output = arrayOperations.ReverseArray(productIds);
+
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent(string.Format("[{0}]", string.Join(", ", output)), Encoding.UTF8, "text/html")
+            };
+        }
+
+        /// <summary>
+        /// Http get method for deleting array element at specified position.
+        /// </summary>
+        /// <param name="position">position</param>
+        /// <param name="productIds">productIds</param>
+        /// <returns>Modified Array</returns>
+        [HttpGet]
+        [Route("DeletePart")]
+        public HttpResponseMessage DeletePart(int position, [FromUri] int[] productIds)
+        {
+            if (productIds == null)
+                return new HttpResponseMessage()
+                {
+                    Content = new StringContent("", Encoding.UTF8, "text/html")
+                };
+
+            var output = arrayOperations.DeleteAtPosition(position, productIds);
+
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent(string.Format("[{0}]", string.Join(", ", output)), Encoding.UTF8, "text/html")
+            };
+        }
+    }
+}
---
+++ ArrayCalcService/ArrayOperations.cs
@@ -0,0 +1,78 @@
+﻿using ArrayCalcContracts;
+using System.Collections.Generic;
+
+namespace ArrayCalcService
+{
+    /// <summary>
+    /// Array Operations Class
+    /// </summary>
+    public class ArrayOperations : IArrayOperations
+    {
+        /// <summary>
+        /// Reverses an input array.
+        /// </summary>
+        /// <param name="inputArray">arraylist</param>
+        public int[] ReverseArray(int[] inputArray)
+        {
+            //Assigning the input to a new array in order to avoid input modification.
+            var outputArray = inputArray;
+
+            //no operation if array is empty or has a single item
+            if (outputArray == null || outputArray.Length < 2)
+            {
+                return outputArray;
+            }
+
+            int lowerbound = default(int);
+            int upperbound = outputArray.Length - 1;
+
+            //interchange items to reverse the arraylist
+            while (lowerbound < upperbound)
+            {
+                var temp = outputArray[lowerbound];
+                outputArray[lowerbound] = outputArray[upperbound];
+                outputArray[upperbound] = temp;
+                lowerbound++;
+                upperbound--;
+            }
+
+            return outputArray;
+        }
+
+        /// <summary>
+        /// Removes an element from input array at specified position.
+        /// </summary>
+        /// <param name="deletePosition">deletePosition</param>
+        /// <param name="inputArray">inputArray</param>
+        /// <returns>Modified Array</returns>
+        public int[] DeleteAtPosition(int deletePosition, int[] inputArray)
+        {
+            if (inputArray == null)
+                return inputArray;
+
+            if (deletePosition <= default(int))
+                return inputArray;
+
+            //subtracting 1 from deletePosition as Arrays are zero index based 
+            deletePosition--;
+
+            //no operation if deletePosition is out of range from arraylist length
+            if (deletePosition >= inputArray.Length)
+            {
+                return inputArray;
+            }
+
+            var processedList = new List<int>();
+
+            //Adding array items to a new list except deleteposition item 
+            for (int id = 0; id < inputArray.Length; id++)
+            {
+                if (id != deletePosition)
+                {
+                    processedList.Add(inputArray[id]);
+                }
+            }
+            return processedList.ToArray();
+        }
+    }
+}
